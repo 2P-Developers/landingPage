afbd6b7f2769d8455a3cc43629cd937bc14fc035
Your Nameqq <you@example.com>
2022-08-22 17:46:55 +0300

    add java script to page

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+// import { toast } from "./node_modules/toastr/toastr.js"
+
+
 let userName = document.getElementById("name");
 let userNumber = document.getElementById("phone-number");
+
+async function readData() {
+    const response = await fetch("offers.json");
+    let myData = await response.json();
+    return myData;
+}
 
 
 if (!window.localStorage.getItem("formfilledCounter")) {
@@ -23,7 +32,9 @@ document.forms[0].onsubmit = function (e) {
             e.preventDefault();
         } else {
             window.localStorage.setItem("formfilledCounter", Number(window.localStorage.getItem("formfilledCounter")) + 1);
+            // toastr.success('Success messages');
             window.alert("تم أرسال الطلب بنجاح....");
+
         }
     }
     else {
@@ -56,10 +67,49 @@ function numberValidate(userNumber) {
         return false;
     }
     if (!reNumber.test(userNumber.value)) {
-        window.alert("يجب ادخال أرقام فقط وعدد الأرقام 10 ");
+        window.alert("يجب ادخال أرقام فقط وعدد الأرقام يجب أن يكون 10 ");
         return false;
     }
     else {
         return true;
     }
 }
+// comboBox 
+async function fillComboBox() {
+    let comboBox = document.getElementById("comboBox");
+    let data = await readData();
+
+    Object.keys(data).forEach((key) => {
+        let optgroup = document.createElement("optgroup");
+        optgroup.label = key;
+        comboBox.appendChild(optgroup);
+
+        data[key].forEach((element) => {
+            let option = document.createElement("option");
+            option.text = element.title;
+            comboBox.appendChild(option);
+        })
+
+    });
+}
+fillComboBox();
+
+async function getUserCountry() {
+    const response = await fetch("https://api.db-ip.com/v2/free/self");
+    let myData = await response.json();
+    return myData;
+}
+async function getCountryPhoneCode() {
+    const response = await fetch("CountryCodes.json");
+    let myData = await response.json();
+    let userCountryCode = await getUserCountry();
+
+    let countryPhoneCode = myData.find((element) => element.code === userCountryCode.countryCode).dial_code;
+    let countryPhoneCodeDiv = document.getElementById("country-code");
+    let myText = document.createTextNode(`${userCountryCode.countryCode} : ${countryPhoneCode}`);
+    countryPhoneCodeDiv.appendChild(myText);
+
+    let countryFlag = document.getElementById("country-flag");
+    countryFlag.src = `https://www.countryflagsapi.com/png/${userCountryCode.countryCode}`;
+}
+getCountryPhoneCode();
